2a641c73666bb5ff681e819b1dd6d8f153d2a46c
tushrx07 <tushrrxx@gmail.com>
2025-04-01 18:50:10 +0530

    change bg

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <h1 className=' w-[500px] p-10 m-auto text-2xl text-blue-400 bg-black text-center rounded-xl mt-10'> An e-commerce website built using ReactJS is an online store where customers can browse, select, and purchase products, with the entire front-end interface developed using the React JavaScript library, allowing for a dynamic and user-friendly shopping experience thanks to its component-based architecture, fast rendering capabilities, and ability to handle complex interactions efficiently; essentially providing a smooth and responsive shopping platform with features like product filtering, adding items to a cart, secure checkout, and user account management, all managed within the React framework. </h1>
+        <h1 className=' w-[500px] p-10 m-auto text-2xl text-blue-400 bg-amber-700 text-center rounded-xl mt-10'> An e-commerce website built using ReactJS is an online store where customers can browse, select, and purchase products, with the entire front-end interface developed using the React JavaScript library, allowing for a dynamic and user-friendly shopping experience thanks to its component-based architecture, fast rendering capabilities, and ability to handle complex interactions efficiently; essentially providing a smooth and responsive shopping platform with features like product filtering, adding items to a cart, secure checkout, and user account management, all managed within the React framework. </h1>
       
     </div>
   )
